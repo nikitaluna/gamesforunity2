03c51a13fe1e74e1e3d0fd3296ced740d82ef9b3
Nikita Ramaswamy <nikita.ramaswamy@gmail.com>
2018-06-09 12:40:41 -0500

    I finished the lesson

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] private Rigidbody2D _rigidbody; 
+	[SerializeField] private float _moveForce;
+	[SerializeField] private float _maxSpeed; 
+
+
+	void FixedUpdate()
+	{
+		 float axis; 
+		axis = Input.GetAxis ("Horizontal"); 
+		 Vector2 force = new Vector2(axis * _moveForce, 0);
+		_rigidbody.AddForce(force);
+
+		if (Mathf.Abs (_rigidbody.velocity.x) > _maxSpeed)
+		 {
+			_rigidbody.velocity = Mathf.Sign (_rigidbody.velocity.x) * new Vector2 (_maxSpeed, _rigidbody.velocity.y);
+				
+		}	
+	
+
+	
+
+ }
+ 
+}
