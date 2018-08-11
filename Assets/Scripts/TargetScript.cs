b4a5f8756e66cbf6ac288dd384b080f8d365a084
Nikita Ramaswamy <nikita.ramaswamy@gmail.com>
2018-08-11 12:11:18 -0500

    Added assets

--- Assets/Scripts/TargetScript.cs
+++ Assets/Scripts/TargetScript.cs
@@ -9,14 +9,17 @@ public class TargetScript : MonoBehaviour {
 	public float ymin;
 	public float ymax;
 	public ScorekeeperScript scoreKeeper;
+	public ParticleSystem Effect; 
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		//Debug.Log("OnCollisionEnter2D");
 		if (collision.gameObject.tag == "Ball") {
+			Instantiate (Effect, transform.position, Quaternion.identity);
 			Instantiate(gameObject, new Vector3(Random.Range(xmin,xmax), Random.Range(ymin,ymax), 0f), Quaternion.identity);
 			scoreKeeper.increaseScore ();
 			Destroy (gameObject);
+
 
 
 		}
